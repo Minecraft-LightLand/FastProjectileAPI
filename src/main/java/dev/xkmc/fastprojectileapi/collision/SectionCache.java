2ab952b21473bfebabf0e05a485c34aa6ca13a42
lcy0x1 <chengyilin@umail.ucsb.edu>
2025-04-10 10:51:11 +0800

    part

--- src/main/java/dev/xkmc/fastprojectileapi/collision/SectionCache.java
+++ src/main/java/dev/xkmc/fastprojectileapi/collision/SectionCache.java
@@ -27,13 +27,13 @@ class SectionCache {
 	private static SectionCache ofServer(ServerLevel sl, int x, int y, int z, ChoiceType type) {
 		var manager = ((ServerLevelAccessor) sl).getEntityManager();
 		var storage = ((PersistentEntitySectionManagerAccessor<Entity>) manager).getSectionStorage();
-		return new SectionCache(storage, x, y, z, type);
+		return new SectionCache(sl, storage, x, y, z, type);
 	}
 
 	private static SectionCache ofClient(Level cl, int x, int y, int z, ChoiceType type) {
 		var manager = ((ClientLevelAccessor) cl).getEntityStorage();
 		var storage = ((TransientEntitySectionManagerAccessor<Entity>) manager).getSectionStorage();
-		return new SectionCache(storage, x, y, z, type);
+		return new SectionCache(cl, storage, x, y, z, type);
 	}
 
 	private final ChoiceType type;
@@ -41,12 +41,15 @@ class SectionCache {
 	private final List<Entity> all = new ArrayList<>();
 	private final List<Entity> margin = new ArrayList<>();
 
-	SectionCache(EntitySectionStorage<Entity> storage, int x, int y, int z, ChoiceType type) {
+	SectionCache(Level level, EntitySectionStorage<Entity> storage, int x, int y, int z, ChoiceType type) {
 		this.type = type;
 		aabb = new AABB(x << 4, y << 4, z << 4,
 				(x + 1) << 4, (y + 1) << 4, (z + 1) << 4);
 		var sect = storage.getSection(SectionPos.asLong(x, y, z));
 		if (sect != null) sect.getEntities().forEach(this::add);
+		for (var e : level.getPartEntities()) {
+			add(e);
+		}
 	}
 
 	private void add(Entity e) {
